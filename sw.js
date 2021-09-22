c84ff3ae20e7c98d55cf0ffbf4c23fa1e46bc8bf
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-09-22 16:56:19 +0900

    キャッシュファイルにBootstrap関連ファイルを追加

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
 importScripts("./src/files.js")
+const CB_ALL_FILES_URLS = CB_ALL_FILES.concat(
+  "https://cdn.honokak.osaka/honoka/4.3.1/css/bootstrap.min.css",
+  "https://code.jquery.com/jquery-3.5.1.slim.min.js",
+  "https://cdn.honokak.osaka/honoka/4.3.1/js/bootstrap.bundle.min.js"
+);
 
 self.addEventListener('fetch', (event) => {
   console.log('service worker fetch ... ' + event.request);

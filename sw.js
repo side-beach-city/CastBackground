a11ef5a7169cc99f081ab2fb1a83a15f7fe34c19
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-12-31 21:27:44 +0900

    キャッシュ対応機構を削除。

--- sw.js
+++ sw.js
@@ -1,32 +1,9 @@
-importScripts("./src/files.js")
-const CB_ALL_FILES_URLS = CB_ALL_FILES.concat(
-  "https://cdn.honokak.osaka/honoka/4.3.1/css/bootstrap.min.css",
-  "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-  "https://cdn.honokak.osaka/honoka/4.3.1/js/bootstrap.bundle.min.js"
-);
-
 self.addEventListener('fetch', (event) => {
   console.log('service worker fetch ... ' + event.request);
-  event.respondWith(
-    caches.match(event.request).then((cacheResponse) => {
-      return cacheResponse || fetch(event.request).then((response) => {
-        return caches.open('v1').then((cache) => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
 });
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll(
-        CB_ALL_FILES
-      );
-    })
-  );
+  console.info('install', event);
 });
 
 self.addEventListener('activate', (event) => {
